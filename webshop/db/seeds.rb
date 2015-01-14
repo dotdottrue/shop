49d9521bc0b1467744eab72afda12a1349d58ef2
tobias kappert <tobias.kappert@web.de>
2015-01-14 12:36:20 +0100

    add model category, product, product_category and products controller. Also added index route and some seeds

--- webshop/db/seeds.rb
+++ webshop/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Category.create(name: 'Software')
+Category.create(name: 'Hardware')
+Category.create(name: 'Kleidung')
