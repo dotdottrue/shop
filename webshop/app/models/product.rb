9dc565ae0e7429b38136f0570af3e9c4f84547c1
tobias kappert <tobias.kappert@web.de>
2015-01-14 13:04:20 +0100

    edit syntax errors

--- webshop/app/models/product.rb
+++ webshop/app/models/product.rb
@@ -2,5 +2,5 @@ class Product < ActiveRecord::Base
   has_many :categories, through: :product_categories
   has_many :product_categories
 
-  validates :name, :description, :price, presende: true
+  validates :name, :description, :price, presence: true
 end
