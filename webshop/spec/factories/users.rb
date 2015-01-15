79887769c78cbbf83b2e3d27b04dff87a144d2b8
tobias kappert <tobias.kappert@web.de>
2015-01-15 11:02:04 +0100

    create branche tobiundeddy and add users scaffold

---
+++ webshop/spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    firstname "MyString"
+lastname "MyString"
+birthday "2015-01-14"
+  end
+
+end
---
+++ webshop/spec/views/users/new.html.erb_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "users/new", :type => :view do
+  before(:each) do
+    assign(:user, User.new(
+      :firstname => "MyString",
+      :lastname => "MyString"
+    ))
+  end
+
+  it "renders new user form" do
+    render
+
+    assert_select "form[action=?][method=?]", users_path, "post" do
+
+      assert_select "input#user_firstname[name=?]", "user[firstname]"
+
+      assert_select "input#user_lastname[name=?]", "user[lastname]"
+    end
+  end
+end
