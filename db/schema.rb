e8fd895980f9cecc175c8460016a181947715e4a
tobias kappert <tobias.kappert@web.de>
2015-02-18 19:26:12 +0100

    change styling/formatting

--- db/schema.rb
+++ db/schema.rb
@@ -32,7 +32,6 @@ ActiveRecord::Schema.define(version: 20150218183021) do
   create_table "products", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
-    t.string   "image"
     t.decimal  "price"
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
