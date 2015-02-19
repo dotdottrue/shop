ba178510936e6c6db6e7dbfb3106990f6a5cff64
tobias kappert <tobias.kappert@web.de>
2015-02-19 21:09:43 +0100

    added a cart link, edit views and controller functions

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,9 +4,9 @@ class Product < ActiveRecord::Base
   has_many :categories, through: :product_categories
   has_many :product_categories
 
-  validates :name, :description, :price, presence: true
+  validates :name, :description, :price, :avatar, presence: true
 
-  has_attached_file :avatar, :styles => { :medium => "300x300", :small => "200x200" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :avatar, :styles => { :medium => "300x300", :small => "200x200", :mini => "100x100" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
 private
