21496bf8582b15e1899924c0436934d440cfcde6
tobias kappert <tobias.kappert@web.de>
2015-02-19 23:43:42 +0100

    edit devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_filter :configure_permitted_parameters, if: :devise_controller?
+  # before_filter :configure_permitted_parameters, if: :devise_controller?
 
-  protected
+  # protected
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :firstname, :street, :zipcode, :email, :password, :current_password) }
-  end
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password) }
+  #   devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :firstname, :street, :zipcode, :email, :password, :current_password) }
+  # end
 
   def current_cart
       Cart.find(session[:cart_id])
