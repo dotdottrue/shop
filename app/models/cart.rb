0b5b20cc2d83d6ceb8097203115b6524197e1bfd
wannabeapro <dennis-russ@web.de>
2015-02-19 13:27:04 +0100

    Cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,6 +2,7 @@ class Cart < ActiveRecord::Base
   has_many :line_items, :dependent => :destroy
   def add_product(product_id)
     current_item = line_items.find_by_product_id(product_id)
+    #die Quantity auf nil prüfen und notfalls mit dem Wert 1 befüllen
     if current_item
       tmp_quantity = current_item.quantity
       if tmp_quantity.nil?
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -3,6 +3,7 @@ class LineItem < ActiveRecord::Base
   belongs_to :cart
 
   def total_price
+    #die Quantity auf nil prüfen und notfalls mit dem Wert 1 befüllen
       tmp_quantity = quantity
       if tmp_quantity.nil?
         tmp_quantity = 1
