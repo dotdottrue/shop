e75100372e16213b87aa40694362a394fc19943b
tobias kappert <tobias.kappert@web.de>
2015-02-23 14:08:52 +0100

    edit product show

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,7 @@ class Order < ActiveRecord::Base
   has_one :user
   has_one :cart
 
-  enum status: [:process, :payment, :shipping]
+  enum status: [:process, :payment, :shipping, :canceled]
 
   
 end
