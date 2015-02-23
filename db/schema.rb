068ff8e39242411f44a16bd9908cdd28d6f01e38
Eduard <es1989@gmail.com>
2015-02-23 20:09:26 +0100

    Added Information to Footer

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150223145745) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -49,8 +52,8 @@ ActiveRecord::Schema.define(version: 20150223145745) do
     t.datetime "updated_at",  null: false
   end
 
-  add_index "product_categories", ["category_id"], name: "index_product_categories_on_category_id"
-  add_index "product_categories", ["product_id"], name: "index_product_categories_on_product_id"
+  add_index "product_categories", ["category_id"], name: "index_product_categories_on_category_id", using: :btree
+  add_index "product_categories", ["product_id"], name: "index_product_categories_on_product_id", using: :btree
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -63,7 +66,7 @@ ActiveRecord::Schema.define(version: 20150223145745) do
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
     t.text     "short_description"
-    t.float    "vat_rate"
+    t.decimal  "vat_rate"
     t.boolean  "in_stock",                                    default: true
   end
 
@@ -98,7 +101,7 @@ ActiveRecord::Schema.define(version: 20150223145745) do
     t.boolean  "admin",                  default: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
