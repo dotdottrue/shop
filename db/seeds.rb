be53c378191e8b6b45b6817ca7b275f3ff4ba275
tobias kappert <tobias.kappert@web.de>
2015-02-24 14:42:50 +0100

    edit seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ Category.create!(name: 'Körner')
 Category.create!(name: 'Brötchen')
 
 ShippingMethod.create(name: 'Standard', duration: '3 Tage', price: 3.50, vat_rate: 7)
-ShippingMethod.create(name: 'Expresslieferung', duration: '1 Tag',price: 3.50, vat_rate: 7)
+ShippingMethod.create(name: 'Expresslieferung', duration: '1 Tag',price: 8.50, vat_rate: 7)
 ShippingMethod.create(name: 'Abholung', duration: 'Dauer bis zur Abholung', price: 0, vat_rate: 7)
 
 User.create!(email: 'admin@dotdottrue.com', password: 'admin678', firstname: 'Administrator', admin: true)
