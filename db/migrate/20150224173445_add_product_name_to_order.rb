5d0c777d4db256a662bf070d9a19553a06dc10ab
tobias kappert <tobias.kappert@web.de>
2015-02-25 12:31:14 +0100

    add migrations

--- db/migrate/20150224173445_add_product_name_to_order.rb
+++
@@ -1,7 +0,0 @@
-class AddProductNameToOrder < ActiveRecord::Migration
-  def change
-  	add_column :orders, :product_name, :string 
-  	add_column :orders, :quantity, :integer
-  	add_column :orders, :product_price, :decimal
-  end
-end
