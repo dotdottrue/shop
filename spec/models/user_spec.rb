e7677e422b9fc03bd17817106c03aef6b1baf4c0
wannabeapro <dennis-russ@web.de>
2015-02-25 13:16:59 +0100

    rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,14 @@
-require 'rspec'
+#require 'rails_helper'
+#require 'rspec'
 require 'spec_helper'
 
 
 describe 'User' do
 
-  it "is valid with a firstname, lastname and Email"
+  it "is valid with a firstname, lastname, password and Email"
   it "is invalid without a firstname"
   it "is invalid without a lastname"
-  it "is iinvalid without a Email"
-
+  it "is invalid without a Email"
+  it "is invalid without a password"
 
 end
