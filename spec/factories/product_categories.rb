cf2564fc5b6fd6fe92383b21919a026c544ba035
Nicbertyo <nchrist@example.com>
2015-02-26 20:57:53 +0100

    restliche Modeltests

--- spec/factories/product_categories.rb
+++ spec/factories/product_categories.rb
@@ -1,7 +1,11 @@
 FactoryGirl.define do
   factory :product_category do
+
+  	product_id 1
+    category_id 2
+    #created_at
+    #updated_at
   	
-    
   end
 
 end
--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
-RSpec.describe Cart, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+describe Cart do
+
+	# setup
+	cart = FactoryGirl.build(:cart)
+  
+  it 'returns the concatenated cart date' do
+    
+    # excercise and verify
+    expect(cart.created_at).to eq DateTime.new(2001,2,3)
+  end
+
+  it 'returns the concatenated cart update date' do
+    
+    # excercise and verify
+    expect(cart.updated_at).to eq DateTime.new(2001,2,4)
+  end
+
+  it 'returns the concatenated default value for the payment_id' do
+    
+    # excercise and verify
+    expect(cart.payment_id).to eq 1
+  end
+
+  it 'returns the concatenated default value for the shipping_method_id' do
+    
+    # excercise and verify
+    expect(cart.shipping_method_id).to eq 1
+  end
+
+  it 'returns the concatenated default value for the optional_invoice_address' do
+    
+    # excercise and verify
+    expect(cart.optional_invoice_address).to eq false
+  end
+
 end
