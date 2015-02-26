b9d39f44e3984055cd869a363e7b09c27e142695
Nicbertyo <nchrist@example.com>
2015-02-26 19:42:21 +0100

    weitere Modelexamples

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,11 +7,28 @@ require 'rails_helper'
 describe User, '#name' do
   it 'returns the concatenated first and last name' do
     # setup
-    # user = build(:user, firstname: 'Josh', lastname: 'Steiner')
     user = FactoryGirl.build(:user)
 
     # excercise and verify
     expect(user.firstname).to eq 'Tobias'
     expect(user.lastname).to eq 'Kappert'
   end
+
+  it 'returns the concatenated password' do
+    # setup
+    user = FactoryGirl.build(:user)
+
+    # excercise and verify
+    expect(user.password).to eq 'tk12345678'
+  end
+
+  it 'returns the concatenated email' do
+    # setup
+    user = FactoryGirl.build(:user)
+
+    # excercise and verify
+    expect(user.email).to eq 'tk@yahoo.de'
+  end
+
+
 end
