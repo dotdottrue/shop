367bb5748a26fb8338fbe4f9a76c90834ae559cd
Nicbertyo <nchrist@example.com>
2015-02-26 20:31:05 +0100

    Product Model rspec

--- spec/factories/product_categories.rb
+++ spec/factories/product_categories.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :product_category do
+  	
     
   end
 
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,16 +4,22 @@ describe Product do
 	# setup
 	product = FactoryGirl.build(:product)
   
-  it 'returns the concatenated productname' do
+  it 'returns the concatenated product name' do
     
     # excercise and verify
     expect(product.name).to eq 'Mein Produkt'
   end
 
-  it 'returns the concatenated productdescription' do
+  it 'returns the concatenated product description' do
 
     # excercise and verify
     expect(product.description).to eq 'Meine Beschreibung'
+  end
+
+  it 'returns the concatenated product price' do
+  	examplePrice = BigDecimal.new('9.99') 
+    # excercise and verify
+    expect(product.price).to eq examplePrice
   end
 
 end
